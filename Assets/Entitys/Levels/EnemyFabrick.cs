e2ced8de4ac7330a6cea3c8ec9a7a64b3c2784df
alastor <114815838+Alastor606@users.noreply.github.com>
2023-09-14 15:07:00 +0300

    Update EnemyFabrick.cs

--- Assets/Entitys/Levels/EnemyFabrick.cs
+++ Assets/Entitys/Levels/EnemyFabrick.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System;
+using static UnityEngine.EventSystems.EventTrigger;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 public class EnemyFabrick : MonoBehaviour
 {
@@ -9,7 +11,9 @@ public class EnemyFabrick : MonoBehaviour
     [SerializeField] private FabrickController _controller;
     [Header("Enemy")]
     [SerializeField] private BossMark _boss;
-    [SerializeField] private List<EnemyStats> _enemyPrefabs;
+    [SerializeField] private List<EnemyStats> _easyEnemy;
+    [SerializeField] private List<EnemyStats> _mediumEnemy;
+    [SerializeField] private List<EnemyStats> _hardEnemy;
     [Header("Stats")]
     [SerializeField] private Vector2 _spawnArea;
     [SerializeField, Range(0.5f,3)] private float _interval;
@@ -17,10 +21,38 @@ public class EnemyFabrick : MonoBehaviour
     private bool _isSpawning = false;
     private int _currentLevel = 0;
 
+    private EnemyStats RandomEasyEnemy()
+    { return _easyEnemy[UnityEngine.Random.Range(0, _easyEnemy.Count - 1)]; }
+
+    private EnemyStats RandomHardEnemy()
+    { return _hardEnemy[UnityEngine.Random.Range(0, _hardEnemy.Count - 1)]; }
+
+    private EnemyStats RandomMediumEnemy()
+    { return _mediumEnemy[UnityEngine.Random.Range(0, _mediumEnemy.Count - 1)]; }
+
     private void Awake()
     {
         _controller.OnLevelStarted += SpawnEnemy;
         _controller.OnLevelFinished += () => _isSpawning = false;
+    }
+
+    private EnemyStats ChooseEnemy()
+    {
+        if (_currentLevel <= 4)
+            return RandomEasyEnemy();
+        else if (_currentLevel > 4 && _currentLevel <= 7)
+        {
+            var random = UnityEngine.Random.Range(0,1);
+            if (random == 0) return RandomEasyEnemy();
+            else return RandomMediumEnemy();
+        }
+        else
+        {
+            var random = UnityEngine.Random.Range(0, 2);
+            if (random == 0) return RandomEasyEnemy();
+            else if (random == 1) return RandomMediumEnemy();
+            else return RandomHardEnemy();
+        }
     }
 
     private async void SpawnEnemy()
@@ -31,9 +63,8 @@ public class EnemyFabrick : MonoBehaviour
             for (int i = 0; i < _enemyPerSpawn; i++)
             {
                 Vector2 spawnPoint = new Vector2(UnityEngine.Random.Range(-_spawnArea.x / 2f, _spawnArea.x / 2f),
-                UnityEngine.Random.Range(-_spawnArea.y / 2f, _spawnArea.y / 2f));
-                var enemy = _enemyPrefabs[UnityEngine.Random.Range(0, _currentLevel)];
-                Instantiate(enemy, spawnPoint, Quaternion.identity);
+                UnityEngine.Random.Range(-_spawnArea.y / 2f, _spawnArea.y / 2f));    
+                Instantiate(ChooseEnemy(), spawnPoint, Quaternion.identity);
             }
             await Task.Delay(TimeSpan.FromSeconds(_interval));
         }
